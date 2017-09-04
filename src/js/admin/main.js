1b8e20b8c39f252a488dd7653f86def3a086c4c5
liaopeng541 <liaopeng541@outlook.com>
2017-09-04 18:02:41 +0800

    修改右边菜单

--- src/js/admin/main.js
+++ src/js/admin/main.js
@@ -18,61 +18,73 @@ export default class Login extends React.Component {
 
     render() {
         return (
-            <Layout style={{minHeight:"100vh"}}>
+            <Layout style={{minHeight: "100vh"}}>
                 <Header className="header">
-                    <div className="logo" />
+                    <div className="logo"/>
                     <Menu
                         theme="dark"
                         mode="horizontal"
                         defaultSelectedKeys={['2']}
-                        style={{ lineHeight: '64px' }}
+                        style={{lineHeight: '64px'}}
                     >
-                        <Menu.Item key="1">nav 1</Menu.Item>
-                        <Menu.Item key="2">nav 2</Menu.Item>
-                        <Menu.Item key="3">nav 3</Menu.Item>
+                        <Menu.Item key="1">主菜单1</Menu.Item>
+                        <Menu.Item key="2">主菜单2</Menu.Item>
+                        <Menu.Item key="3">主菜单3</Menu.Item>
                     </Menu>
                 </Header>
                 <Layout>
-                    <Sider width={200} style={{ background: '#fff' }}>
-                        <Menu
-                            mode="inline"
-                            defaultSelectedKeys={['1']}
-                            defaultOpenKeys={['sub1']}
-                            style={{ height: '100%', borderRight: 0 }}
-                        >
-                            <SubMenu key="sub1" title={<span><Icon type="user" />subnav 1</span>}>
-                                <Menu.Item key="1">option1</Menu.Item>
-                                <Menu.Item key="2">option2</Menu.Item>
-                                <Menu.Item key="3">option3</Menu.Item>
-                                <Menu.Item key="4">option4</Menu.Item>
+                    <Sider collapsable
+                           trigger={null}
+                           collapsed={this.state.collapsed} width={200} style={{background: '#fff'}}>
+
+                        <Menu defaultSelectedKeys={['1']} mode="inline" style={{height:"100%",border:"none"}}>
+
+                            <SubMenu
+                                key="sub1"
+                                title={<span><Icon type="user" /><span>系统设置</span></span>}
+                            >
+                                <Menu.Item key="3">Tom</Menu.Item>
+                                <Menu.Item key="4">Bill</Menu.Item>
+                                <Menu.Item key="5">Alex</Menu.Item>
                             </SubMenu>
-                            <SubMenu key="sub2" title={<span><Icon type="laptop" />subnav 2</span>}>
-                                <Menu.Item key="5">option5</Menu.Item>
-                                <Menu.Item key="6">option6</Menu.Item>
-                                <Menu.Item key="7">option7</Menu.Item>
-                                <Menu.Item key="8">option8</Menu.Item>
+                            <SubMenu
+                                key="sub2"
+                                title={<span><Icon type="team" /><span>权限管理</span></span>}
+                            >
+                                <Menu.Item key="6">Team 1</Menu.Item>
+                                <Menu.Item key="8">Team 2</Menu.Item>
                             </SubMenu>
-                            <SubMenu key="sub3" title={<span><Icon type="notification" />subnav 3</span>}>
-                                <Menu.Item key="9">option9</Menu.Item>
-                                <Menu.Item key="10">option10</Menu.Item>
-                                <Menu.Item key="11">option11</Menu.Item>
-                                <Menu.Item key="12">option12</Menu.Item>
-                            </SubMenu>
+                            <Menu.Item key="9">
+                                <Icon type="file" />
+                                <span>File</span>
+                            </Menu.Item>
+                            <Menu.Item key="1">
+                                <Icon type="pie-chart" />
+                                <span>Option 1</span>
+                            </Menu.Item>
+                            <Menu.Item key="2">
+                                <Icon type="desktop" />
+                                <span>Option 2</span>
+                            </Menu.Item>
                         </Menu>
                     </Sider>
-                    <Layout style={{flexDirection:"row"}}>
-                        <div style={{height:"100%", width:"15px", justifyContent:"center", alignItems:"center",backgroundColor: "#cc0033"}}>
-                            <h1>}</h1></div>
-                    <Layout>
-                        <Breadcrumb style={{ margin: '12px 0' }}>
-                            <Breadcrumb.Item>Home</Breadcrumb.Item>
-                            <Breadcrumb.Item>List</Breadcrumb.Item>
-                            <Breadcrumb.Item>App</Breadcrumb.Item>
-                        </Breadcrumb>
-                        <Content style={{ background: '#fff', padding: 24, margin: 0, minHeight: 280 }}>
-                            Content
-                        </Content>
-                    </Layout>
+                    <Layout style={{flexDirection: "row"}}>
+                        <div style={{height: "100%", width: "16px", justifyContent: "center",}}>
+
+                                <Icon type={this.state.collapsed ? 'caret-right' : 'caret-left'}
+                                      onClick={()=>{this.setState({collapsed:!this.state.collapsed})}} className="coll-img"/>
+
+                        </div>
+                        <Layout>
+                            <Breadcrumb style={{margin: '12px 0'}}>
+                                <Breadcrumb.Item>Home</Breadcrumb.Item>
+                                <Breadcrumb.Item>List</Breadcrumb.Item>
+                                <Breadcrumb.Item>App</Breadcrumb.Item>
+                            </Breadcrumb>
+                            <Content style={{background: '#fff', padding: 24, margin: 0, minHeight: 280}}>
+                                Content
+                            </Content>
+                        </Layout>
                     </Layout>
                 </Layout>
             </Layout>
