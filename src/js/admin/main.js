7ce9616cebedb48ee1e87054c1c5d6a43ab7008c
liaopeng541 <36416976@qq.com>
2017-09-05 08:58:57 +0800

    更新

--- src/js/admin/main.js
+++ src/js/admin/main.js
@@ -3,10 +3,22 @@
  */
 import React from 'react';
 import ReactDOM from "react-dom"
-import {Layout, Menu, Breadcrumb, Button, Icon} from 'antd';
+import {Layout, Menu, Breadcrumb, Dropdown, Button, Row, Col, Avatar, Icon} from 'antd';
 const {Header, Content, Footer, Sider} = Layout;
 const SubMenu = Menu.SubMenu;
 import  "./main.less"
+const menu = (
+    <Menu>
+        <Menu.Item>
+            修改密码
+        </Menu.Item>
+        <Menu.Item>
+            退出
+        </Menu.Item>
+
+    </Menu>
+);
+
 export default class Login extends React.Component {
     constructor(props) {
         super(props);
@@ -19,29 +31,61 @@ export default class Login extends React.Component {
     render() {
         return (
             <Layout style={{minHeight: "100vh"}}>
-                <Header className="header">
-                    <div className="logo"/>
-                    <Menu
-                        theme="dark"
-                        mode="horizontal"
-                        defaultSelectedKeys={['2']}
-                        style={{lineHeight: '64px'}}
-                    >
-                        <Menu.Item key="1">主菜单1</Menu.Item>
-                        <Menu.Item key="2">主菜单2</Menu.Item>
-                        <Menu.Item key="3">主菜单3</Menu.Item>
-                    </Menu>
-                </Header>
+                <Row style={{backgroundColor: "#0d68ab"}}>
+
+                    <Col span="20">
+                        <Menu
+                            theme="dark"
+                            mode="horizontal"
+                            defaultSelectedKeys={['2']}
+                            style={{lineHeight: '64px', backgroundColor: "#0d68ab"}}
+                        >
+                            <Menu.Item key="0" disabled style={{ cursor: "pointer"}}>
+                                <span style={{fontSize: "20px", color:"#ffffff"}}>xxoo管理系统</span>
+                            </Menu.Item>
+                            <Menu.Item key="1">主菜单1111111111111111</Menu.Item>
+                            <Menu.Item key="2">主菜单2</Menu.Item>
+                            <Menu.Item key="3">主菜单3</Menu.Item>
+                        </Menu>
+                    </Col>
+                    <Col span="4">
+                        <Dropdown overlay={menu}>
+                            <div className="headerright">
+                                <Icon type="down"
+                                      style={{fontSize: "16px", float: "right", margin: "8px", color: "#ffffff"}}/>
+                                <Avatar style={{float: "right"}}
+                                        src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png"/>
+
+                                <span style={{
+                                    float: "right",
+                                    height: "32px",
+                                    lineHeight: "32px",
+                                    marginRight: "8px",
+                                    color: "#ffffff"
+                                }}>
+                                廖鹏
+                            </span>
+                            </div>
+                        </Dropdown>
+                    </Col>
+
+
+                </Row>
                 <Layout>
                     <Sider collapsable
                            trigger={null}
                            collapsed={this.state.collapsed} width={200} style={{background: '#fff'}}>
 
-                        <Menu defaultSelectedKeys={['1']} mode="inline" style={{height:"100%",border:"none"}}>
+                        <Menu defaultSelectedKeys={['1']} mode="inline" style={{height: "100%", border: "none"}}>
 
+                            <Menu.Item key="10" disabled style={{backgroundColor: "#108ee9", cursor: "auto"}}>
+
+                                <Icon type="windows" style={{fontSize: "16px", color: "#ffffff"}}/>
+                                <span style={{color: "#ffffff", fontSize: "16px"}}>管理菜单</span>
+                            </Menu.Item>
                             <SubMenu
                                 key="sub1"
-                                title={<span><Icon type="user" /><span>系统设置</span></span>}
+                                title={<span><Icon type="user"/><span>系统设置</span></span>}
                             >
                                 <Menu.Item key="3">Tom</Menu.Item>
                                 <Menu.Item key="4">Bill</Menu.Item>
@@ -49,21 +93,21 @@ export default class Login extends React.Component {
                             </SubMenu>
                             <SubMenu
                                 key="sub2"
-                                title={<span><Icon type="team" /><span>权限管理</span></span>}
+                                title={<span><Icon type="team"/><span>权限管理</span></span>}
                             >
                                 <Menu.Item key="6">Team 1</Menu.Item>
                                 <Menu.Item key="8">Team 2</Menu.Item>
                             </SubMenu>
                             <Menu.Item key="9">
-                                <Icon type="file" />
+                                <Icon type="file"/>
                                 <span>File</span>
                             </Menu.Item>
                             <Menu.Item key="1">
-                                <Icon type="pie-chart" />
+                                <Icon type="pie-chart"/>
                                 <span>Option 1</span>
                             </Menu.Item>
                             <Menu.Item key="2">
-                                <Icon type="desktop" />
+                                <Icon type="desktop"/>
                                 <span>Option 2</span>
                             </Menu.Item>
                         </Menu>
@@ -71,16 +115,32 @@ export default class Login extends React.Component {
                     <Layout style={{flexDirection: "row"}}>
                         <div style={{height: "100%", width: "16px", justifyContent: "center",}}>
 
-                                <Icon type={this.state.collapsed ? 'caret-right' : 'caret-left'}
-                                      onClick={()=>{this.setState({collapsed:!this.state.collapsed})}} className="coll-img"/>
+                            <Icon type={this.state.collapsed ? 'caret-right' : 'caret-left'}
+                                  onClick={() => {
+                                      this.setState({collapsed: !this.state.collapsed})
+                                  }} className="coll-img"/>
 
                         </div>
                         <Layout>
-                            <Breadcrumb style={{margin: '12px 0'}}>
-                                <Breadcrumb.Item>Home</Breadcrumb.Item>
-                                <Breadcrumb.Item>List</Breadcrumb.Item>
-                                <Breadcrumb.Item>App</Breadcrumb.Item>
-                            </Breadcrumb>
+                            <Row>
+                                <Col span="22">
+                                    <Breadcrumb style={{margin: '12px 0'}}>
+                                        <Breadcrumb.Item>Home</Breadcrumb.Item>
+                                        <Breadcrumb.Item>List</Breadcrumb.Item>
+                                        <Breadcrumb.Item>App</Breadcrumb.Item>
+                                    </Breadcrumb>
+                                </Col>
+                                <Col span="2" style={{
+                                    height: "42px",
+                                    lineHeight: "42px",
+                                    textAlign: "right",
+                                    paddingRight: "10px"
+                                }}>
+                                    <Button >
+                                        <Icon type="reload"/>
+                                    </Button>
+                                </Col>
+                            </Row>
                             <Content style={{background: '#fff', padding: 24, margin: 0, minHeight: 280}}>
                                 Content
                             </Content>
