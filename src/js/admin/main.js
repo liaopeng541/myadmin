22e79414c06de8007f5bcb787b65bc904edeec9c
liaopeng541 <36416976@qq.com>
2017-09-13 09:33:12 +0800

    修改了管理界面

--- src/js/admin/main.js
+++ src/js/admin/main.js
@@ -3,9 +3,11 @@
  */
 import React from 'react';
 import ReactDOM from "react-dom"
-import {Layout, Menu, Breadcrumb, Dropdown, Button, Row, Col, Avatar, Icon} from 'antd';
+import {Layout, Menu, Breadcrumb,Tabs,Tooltip, Dropdown, Button, Row, Col, Avatar, Icon} from 'antd';
 const {Header, Content, Footer, Sider} = Layout;
+const TabPane = Tabs.TabPane;
 const SubMenu = Menu.SubMenu;
+import Index from "./pages/index"
 import  "./main.less"
 const menu = (
     <Menu>
@@ -18,15 +20,62 @@ const menu = (
 
     </Menu>
 );
-
 export default class Login extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
             collapsed: false,
+            activeKey: '1',
+            panes:[
+                { title: '首页', content: 'Content of Tab 1', key: '1', closable: false }
+            ]
         };
     }
+    onChange(activeKey){
+        this.setState({ activeKey });
+    }
+    onEdit (targetKey, action){
+        this[action](targetKey);
+    }
+    add(item)
+    {
+        var key=item.key
+        this.state.panes.map((item,i)=>{
+            if(item.key==key)
+            {
+                this.setState({
+                    activeKey:key
+                })
+                return;
+            }
+        })
+        this.state.panes.push({ title: key, content: <Index/>, key: key })
+        this.setState({panes:this.state.panes,activeKey:key},()=>{console.log(this.state.panes)});
+    }
 
+    remove (targetKey) {
+        let activeKey = this.state.activeKey;
+        let lastIndex;
+        this.state.panes.forEach((pane, i) => {
+            if (pane.key === targetKey) {
+                lastIndex = i - 1;
+            }
+        });
+        const panes = this.state.panes.filter(pane => pane.key !== targetKey);
+        if (lastIndex >= 0 && activeKey === targetKey) {
+            activeKey = panes[lastIndex].key;
+        }
+        this.setState({ panes, activeKey });
+    }
+    closeall()
+    {
+        var temppanel=[];
+        temppanel.push(this.state.panes[0])
+        this.setState({
+            activeKey:"1",
+            panes: temppanel
+        })
+    }
 
     render() {
         return (
@@ -43,7 +92,7 @@ export default class Login extends React.Component {
                             <Menu.Item key="0" disabled style={{ cursor: "pointer"}}>
                                 <span style={{fontSize: "20px", color:"#ffffff"}}>xxoo管理系统</span>
                             </Menu.Item>
-                            <Menu.Item key="1">主菜单1111111111111111</Menu.Item>
+                            <Menu.Item key="1" >主菜单1111111111111111</Menu.Item>
                             <Menu.Item key="2">主菜单2</Menu.Item>
                             <Menu.Item key="3">主菜单3</Menu.Item>
                         </Menu>
@@ -76,7 +125,7 @@ export default class Login extends React.Component {
                            trigger={null}
                            collapsed={this.state.collapsed} width={200} style={{background: '#fff'}}>
 
-                        <Menu defaultSelectedKeys={['1']} mode="inline" style={{height: "100%", border: "none"}}>
+                        <Menu onSelect={this.add.bind(this)} defaultSelectedKeys={['1']} mode="inline" style={{height: "100%", border: "none"}}>
 
                             <Menu.Item key="10" disabled style={{backgroundColor: "#108ee9", cursor: "auto"}}>
 
@@ -87,7 +136,7 @@ export default class Login extends React.Component {
                                 key="sub1"
                                 title={<span><Icon type="user"/><span>系统设置</span></span>}
                             >
-                                <Menu.Item key="3">Tom</Menu.Item>
+                                <Menu.Item key="999" ><span><Icon type="user"/><span>Tom111</span></span></Menu.Item>
                                 <Menu.Item key="4">Bill</Menu.Item>
                                 <Menu.Item key="5">Alex</Menu.Item>
                             </SubMenu>
@@ -102,11 +151,11 @@ export default class Login extends React.Component {
                                 <Icon type="file"/>
                                 <span>File</span>
                             </Menu.Item>
-                            <Menu.Item key="1">
+                            <Menu.Item key="888">
                                 <Icon type="pie-chart"/>
                                 <span>Option 1</span>
                             </Menu.Item>
-                            <Menu.Item key="2">
+                            <Menu.Item key="777">
                                 <Icon type="desktop"/>
                                 <span>Option 2</span>
                             </Menu.Item>
@@ -122,28 +171,31 @@ export default class Login extends React.Component {
 
                         </div>
                         <Layout>
-                            <Row>
-                                <Col span="22">
-                                    <Breadcrumb style={{margin: '12px 0'}}>
-                                        <Breadcrumb.Item>Home</Breadcrumb.Item>
-                                        <Breadcrumb.Item>List</Breadcrumb.Item>
-                                        <Breadcrumb.Item>App</Breadcrumb.Item>
-                                    </Breadcrumb>
-                                </Col>
-                                <Col span="2" style={{
-                                    height: "42px",
-                                    lineHeight: "42px",
-                                    textAlign: "right",
-                                    paddingRight: "10px"
-                                }}>
-                                    <Button >
-                                        <Icon type="reload"/>
-                                    </Button>
-                                </Col>
-                            </Row>
-                            <Content style={{background: '#fff', padding: 24, margin: 0, minHeight: 280}}>
-                                Content
+                            <Content style={{background: '#fff',  minHeight: 280,marginTop:"5px"}}>
+                                <Row>
+                                    <Col span={22}>
+                                <Tabs
+                                    onChange={this.onChange.bind(this)}
+                                    activeKey={this.state.activeKey}
+                                    type="editable-card"
+                                    onEdit={this.onEdit.bind(this)}
+                                    hideAdd={true}
+                                    className="card-container"
+                                >
+                                    {this.state.panes.map(pane => <TabPane tab={pane.title} key={pane.key} closable={pane.closable}>
+                                        {pane.content}
+                                    </TabPane>)}
+                                </Tabs>
+                                    </Col>
+                                    <Col span={2} style={{backgroundColor:"#ececec",height:"32px",textAlign:"right",
+                                        lineHeight:"32px", paddingRight:"5px"
+                                    }}><Tooltip title="全部关闭" placement="left">
+                                        <Button shape="circle" icon="close" onClick={this.closeall.bind(this)} size="small"  />
+                                    </Tooltip>
+                                    </Col>
+                                </Row>
                             </Content>
+
                         </Layout>
                     </Layout>
                 </Layout>
